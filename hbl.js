49454ea2108831666764bcc3fc2887ac4319a28b
habilam24 <159699358+habilam24@users.noreply.github.com>
2024-10-08 21:37:40 +0700

    Add files via upload

---
+++ hbl.js
@@ -0,0 +1,9 @@
+//text animation Script
+var typed = new Typed(".typing", {
+    strings: ["Indonesia"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    cursorChar: ' | ',
+    smartBackspace: true,
+    loop: true
+});
